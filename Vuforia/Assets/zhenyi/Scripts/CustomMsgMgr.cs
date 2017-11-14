0b51fcb0907397fb1f92e50876ff39c00d3a8b55
Zhenyi He <snowymohehe@gmail.com>
2017-11-14 02:15:07 -0500

    finish the custom msg of mesh, network part is finished, switch is hololens/scanner in inspector

---
+++ Vuforia/Assets/zhenyi/Scripts/CustomMsgMgr.cs
@@ -0,0 +1,103 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Academy.HoloToolkit.Sharing;
+using Academy.HoloToolkit.Unity;
+
+public class CustomMsgMgr : MonoBehaviour
+{
+
+     Transform modelObj;
+     Transform cameraObj;
+    public Mesh mesh;
+
+    public MeshRender mr;
+
+    public enum Category { Hololens, Scanner };
+
+    public Category category;
+
+    CustomMessages customMessages;
+    //  [SerializeField]
+    //  CustomMessages.TestMessageID id;
+
+    // Use this for initialization
+    void Start()
+    {
+        customMessages = CustomMessages.Instance;
+        //id = customMessages.generateMID();
+        customMessages.MessageHandlers[CustomMessages.TestMessageID.Mesh] = this.receiveMesh;
+        //customMessages.MessageHandlers[CustomMessages.TestMessageID.Camera] = this.receiveCameraTransform;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (category == Category.Hololens)
+        {
+            //sendTransform(CustomMessages.TestMessageID.Camera);
+        }
+        else
+        {
+            sendMesh(CustomMessages.TestMessageID.Mesh);
+        }
+
+    }
+
+    void sendTransform(CustomMessages.TestMessageID id)
+    {
+        CustomMessages.Instance.SendCustomTransform(id, cameraObj.position, cameraObj.rotation);
+        print(cameraObj.position);
+    }
+
+    void sendMesh(CustomMessages.TestMessageID id)
+    {
+        CustomMessages.Instance.SendMesh(mesh);
+        
+    }
+
+    void sendTransform()
+    {
+        CustomMessages.Instance.SendStageTransform(modelObj.position, modelObj.rotation);
+    }
+
+    void receiveModelTransform(NetworkInMessage msg)
+    {
+        // Parse the message
+        long userID = msg.ReadInt64();
+
+        modelObj.position = customMessages.ReadVector3(msg);
+
+        modelObj.rotation = customMessages.ReadQuaternion(msg);
+
+    }
+
+    void receiveMesh(NetworkInMessage msg)
+    {
+        // Parse the message
+        long userID = msg.ReadInt64();
+
+        int triangleLen = msg.ReadInt32();
+        int[] triangles = new int[triangleLen];
+        for (int i = 0; i < triangleLen; i++)
+            triangles[i] = msg.ReadInt32();
+
+        int verticeLen = msg.ReadInt32();
+        Vector3[] vertices = new Vector3[verticeLen];
+        for (int i = 0; i < verticeLen; i++)
+            vertices[i] = customMessages.ReadVector3(msg);
+
+        mr.updateMesh(triangles, vertices);
+    }
+
+    void receiveCameraTransform(NetworkInMessage msg)
+    {
+        // Parse the message
+        long userID = msg.ReadInt64();
+
+        cameraObj.position = customMessages.ReadVector3(msg);
+
+        cameraObj.rotation = customMessages.ReadQuaternion(msg);
+
+    }
+}
---
+++ Vuforia/Assets/zhenyi/Scripts/MeshRender.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MeshRender : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        
+    }
+
+    public void updateMesh(int[] triangles, Vector3[] vertices)
+    {
+        GetComponent<MeshFilter>().mesh.vertices = vertices;
+        GetComponent<MeshFilter>().mesh.triangles = triangles;
+    }
+}
