6ad4ec3ba383f8761bd51800da0e851ae067274d
Zhenyi He <snowymohehe@gmail.com>
2018-01-17 06:24:44 -0500

    now unity can receive float array and uint array for real time mesh. Also find a backup repo for ply too. Mesh is on the way.

--- VuforiaUnity2017.3/Assets/zhenyi/Scripts/PLYLoader.cs
+++ VuforiaUnity2017.3/Assets/zhenyi/Scripts/PLYLoader.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using System.Runtime.InteropServices;
 using System;
+using System.Text;
 
 public class PLYLoader : MonoBehaviour {
 
@@ -13,12 +14,20 @@ public class PLYLoader : MonoBehaviour {
     [DllImport("PLYDecoder", EntryPoint = "LoadPLY")]
     public static extern void LoadPLY(IntPtr F, IntPtr V, IntPtr N, int[] meshSize);
 
+    [DllImport("PLYDecoder", EntryPoint = "LoadPLY2")]
+    public static extern void LoadPLY2(out IntPtr F, out IntPtr V, out IntPtr N, int[] meshSize);
+
+    [DllImport("PLYDecoder", EntryPoint = "LoadPLY3")]
+    public static extern void LoadPLY3([MarshalAs(UnmanagedType.LPStr)] string path, IntPtr[] F, IntPtr[] V, int[] meshSize);
+
     public int[] a;
+
+    public string scanPath;
 
     [SerializeField]
     private int[] cnt;
     [SerializeField]
-    private double[] V, N;
+    private float[] V, N;
     [SerializeField]
     private int[] F;
     // Use this for initialization
@@ -46,9 +55,9 @@ public class PLYLoader : MonoBehaviour {
 
             F = new int[cnt[0]];
             Marshal.Copy(pF, F, 0, cnt[0]);
-            V = new double[cnt[1]];
+            V = new float[cnt[1]];
             Marshal.Copy(pV, V, 0, cnt[1]);
-            N = new double[cnt[2]];
+            N = new float[cnt[2]];
             Marshal.Copy(pN, N, 0, cnt[2]);
 
             //Marshal.Copy(typesize, debuglength, 0, 1);
@@ -63,9 +72,95 @@ public class PLYLoader : MonoBehaviour {
             //Marshal.FreeHGlobal(typesize);
         }
     }
-	
-	// Update is called once per frame
-	void Update () {
+
+    public void loadPLY2()
+    {
+        cnt = new int[3];
+        IntPtr pF = new IntPtr();
+        IntPtr pV = new IntPtr();
+        IntPtr pN = new IntPtr();
+        //IntPtr pCnt = Marshal.AllocHGlobal(sizeof(int) * 3);
+        //IntPtr typesize = Marshal.AllocHGlobal(Marshal.SizeOf(debuglength[0]));
+        //int pCnt = 4;
+        try
+        {
+            // Copy the array to unmanaged memory.
+            //Marshal.Copy(holoPointSet, 0, holopoints, holoPointSet.Length);
+            //Marshal.Copy(vivePointSet, 0, vivepoints, vivePointSet.Length);
+            //Marshal.Copy(debuglength, 0, typesize, 1);
+
+            LoadPLY2(out pF, out pV, out pN, cnt);
+
+            F = new int[cnt[0]];
+            Marshal.Copy(pF, F, 0, cnt[0]);
+            V = new float[cnt[1]];
+            Marshal.Copy(pV, V, 0, cnt[1]);
+            N = new float[cnt[2]];
+            Marshal.Copy(pN, N, 0, cnt[2]);
+
+            //Marshal.Copy(typesize, debuglength, 0, 1);
+            //print(debuglength[0]);
+        }
+        finally
+        {
+            // Free the unmanaged memory.
+            Marshal.FreeHGlobal(pF);
+            Marshal.FreeHGlobal(pV);
+            Marshal.FreeHGlobal(pN);
+            //Marshal.FreeHGlobal(typesize);
+        }
+    }
+
+    public void loadPLY3()
+    {
+        cnt = new int[2];
+        //IntPtr[] pFVN = new IntPtr[2];
+        IntPtr[] pF = new IntPtr[1];
+        pF[0] = new IntPtr(0);
+        IntPtr[] pV = new IntPtr[1];
+        pV[0] = new IntPtr(0);
+        //         for (int i = 0; i < pFVN.Length; i++)
+        //         {
+        //             pFVN[i] = new IntPtr();
+        //             cnt[i] = 0;
+        //         }
+
+        //         IntPtr pF = new IntPtr();
+        //         IntPtr pV = new IntPtr();
+        //         IntPtr pN = new IntPtr();
+        try
+        {
+            //StringBuilder sb = new StringBuilder(scanPath);
+            LoadPLY3(scanPath, pF, pV, cnt);
+
+            if(cnt[0] > 0)
+            {
+                F = new int[cnt[0]];
+                Marshal.Copy(pF[0], F, 0, cnt[0]);
+            }
+            if(cnt[1] > 0)
+            {
+                V = new float[cnt[1]];
+                Marshal.Copy(pV[0], V, 0, cnt[1]);
+            }
+//             if(cnt[2] > 0)
+//             {
+//                 N = new double[cnt[2]];
+//                 Marshal.Copy(pFVN[2], N, 0, cnt[2]);
+//             }
+        }
+        finally
+        {
+            // Free the unmanaged memory.
+            //foreach(IntPtr ip in pFVN)
+                Marshal.FreeHGlobal(pF[0]);
+            Marshal.FreeHGlobal(pV[0]);
+            //Marshal.FreeHGlobal(typesize);
+        }
+    }
+
+    // Update is called once per frame
+    void Update () {
 		
 	}
 }
