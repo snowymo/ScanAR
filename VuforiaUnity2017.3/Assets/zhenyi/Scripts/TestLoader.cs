36049b5dbd323b0553fa6118c78193c96033907c
Zhenyi He <snowymohehe@gmail.com>
2018-01-18 01:59:41 -0500

    add function: load specific current scan and delete after loading

--- VuforiaUnity2017.3/Assets/zhenyi/Scripts/TestLoader.cs
+++ VuforiaUnity2017.3/Assets/zhenyi/Scripts/TestLoader.cs
@@ -2,6 +2,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.IO;
 
 public class TestLoader : MonoBehaviour
 {
@@ -9,10 +10,11 @@ public class TestLoader : MonoBehaviour
     public bool isAbsolute;
     public string filePath;
 
-    // Use this for initialization
-    void Start()
+    public string currentScan;
+
+    void preloadTest()
     {
-        if(!isAbsolute)
+        if (!isAbsolute)
             filePath = System.IO.Path.Combine(Application.streamingAssetsPath, fileName);
         IntPtr plyIntPtr = PlyLoaderDll.LoadPly(filePath);
 
@@ -44,5 +46,41 @@ public class TestLoader : MonoBehaviour
         }
 
         PlyLoaderDll.UnLoadPly(plyIntPtr);
+    }
+
+    void loadPLYwithPtr(IntPtr p)
+    {
+        Mesh mesh = new Mesh();
+        mesh.vertices = PlyLoaderDll.GetVertices(p);
+        //mesh.uv = PlyLoaderDll.GetUvs(plyIntPtr);
+        //mesh.normals = PlyLoaderDll.GetNormals(plyIntPtr);
+        //mesh.colors32 = PlyLoaderDll.GetColors(plyIntPtr);
+        mesh.SetIndices(PlyLoaderDll.GetIndexs(p), MeshTopology.Triangles, 0, true);
+        mesh.name = "mesh";
+
+        GameObject go = new GameObject();
+        go.name = "meshNew";
+        MeshFilter mf = go.AddComponent<MeshFilter>();
+        mf.mesh = mesh;
+        MeshRenderer mr = go.AddComponent<MeshRenderer>();
+        mr.material = new Material(Shader.Find("UCLA Game Lab/Wireframe/Double-Sided"));
+    }
+
+    // Use this for initialization
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        // load file until success and then remove
+        if (File.Exists(currentScan))
+        {
+            IntPtr plyIntPtr = PlyLoaderDll.LoadPly(currentScan);
+            loadPLYwithPtr(plyIntPtr);
+            PlyLoaderDll.UnLoadPly(plyIntPtr);
+            File.Delete(currentScan);
+        }
     }
 }
