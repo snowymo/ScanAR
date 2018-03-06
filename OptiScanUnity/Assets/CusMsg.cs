a13abde32b670df6a77e7087f37fcd56599bc8d3
Zhenyi He <snowymohehe@gmail.com>
2018-03-05 23:33:39 -0500

    OptiScanUnity is setup for unity server receive from holojam to share service; OptiScanHololens is setup for hololens to receive from unity server;

---
+++ OptiScanUnity/Assets/CusMsg.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Academy.HoloToolkit.Sharing;
+using Academy.HoloToolkit.Unity;
+using System;
+
+public class CusMsg : MonoBehaviour {
+
+    CustomMessages customMessages;
+
+    public Transform modelObj;
+
+    public bool isHololens;
+
+    [SerializeField]
+    bool currentPositionTracking;
+
+    // Use this for initialization
+    void Start () {
+        customMessages = CustomMessages.Instance;
+        customMessages.MessageHandlers[CustomMessages.TestMessageID.Transform] = this.receiveTransform;
+
+        UnityEngine.XR.InputTracking.disablePositionalTracking = true;
+        currentPositionTracking = UnityEngine.XR.InputTracking.disablePositionalTracking;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        //switch (UnityEngine.XR.XRSettings.loadedDeviceName)
+       if(!isHololens)
+            sendTransform(CustomMessages.TestMessageID.Transform);
+               
+	}
+
+    private void sendTransform(CustomMessages.TestMessageID id)
+    {
+        CustomMessages.Instance.SendTransform(id, modelObj.position, modelObj.rotation);
+    }
+
+    void receiveTransform(NetworkInMessage msg)
+    {
+        // Parse the message
+        long userID = msg.ReadInt64();
+
+        modelObj.position = customMessages.ReadVector3(msg);
+        modelObj.rotation = customMessages.ReadQuaternion(msg);
+    }
+}
---
+++ OptiScanUnity/Assets/Scripts/Applications/GlobalCtrl.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GlobalCtrl : MonoBehaviour {
+
+    public bool isHololens;
+
+    public GameObject[] hololensRelated;
+
+    public GameObject[] unityRelated;
+
+	// Use this for initialization
+	void Start () {
+
+            foreach(GameObject go in hololensRelated)
+            {
+                go.SetActive(isHololens);
+            }
+            foreach(GameObject go in unityRelated)
+            {
+                go.SetActive(!isHololens);
+            }
+
+        
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
